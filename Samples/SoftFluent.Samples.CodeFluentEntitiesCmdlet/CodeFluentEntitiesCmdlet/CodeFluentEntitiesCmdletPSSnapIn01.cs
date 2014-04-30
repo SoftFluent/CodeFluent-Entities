dd6b389d1147d93739d264b0dc008cf68cc5e2f8
Mracox <marc.lecoz@gmail.com>
2014-04-30 16:23:44 +0200

    Add CodeFluentEntitiesCmdlet sample
    CodeFluentEntitiesCmdlet is a sample on how easily updating several
    common-based databases with the CodeFluent Entities Pivot Script Runner
    tool, abstracted through a Windows Powershell CmdLet.

---
+++ Samples/SoftFluent.Samples.CodeFluentEntitiesCmdlet/CodeFluentEntitiesCmdlet/CodeFluentEntitiesCmdletPSSnapIn01.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Management.Automation;
+
+namespace CodeFluentEntitiesCmdlet
+{
+    [RunInstaller(true)]
+    public class CodeFluentEntitiesCmdletPSSnapIn01 : PSSnapIn
+    {
+        public CodeFluentEntitiesCmdletPSSnapIn01()
+            : base() { }
+
+        public override string Name
+        {
+            get { return ((object)this).GetType().Name; }
+        }
+
+        public override string Vendor
+        {
+            get { return "SoftFluent"; }
+        }
+
+        public override string VendorResource
+        {
+            get { return string.Format("{0},{1}", Name, Vendor); }
+        }
+
+        public override string Description
+        {
+            get { return "This is a PowerShell snap-in that includes Update-CFEDatabases cmdlet."; }
+        }
+
+        public override string DescriptionResource
+        {
+            get { return string.Format("{0},{1}", Name, Description); }
+        }
+    }
+}
