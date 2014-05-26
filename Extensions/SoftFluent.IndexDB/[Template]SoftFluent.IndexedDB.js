9fc4f821d59f7a0b010742d5e9e32cc0aca80c2e
Hugo CARNICELLI <hugocarnicelli@gmail.com>
2014-05-26 18:19:13 +0200

    Fixes functionnal the example of Indexed
    Fixing multiple issues on the producer for CRUD operations.
    Adding callback methods to the functions that can need it.
    Renaming the producer.

--- Extensions/SoftFluent.IndexDB/[Template]SoftFluent.IndexedDB.js
+++ Extensions/SoftFluent.IndexDB/[Template]SoftFluent.IndexedDB.js
@@ -1,15 +1,15 @@
-[%@ reference name="C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.Core.dll" %]
-[%@ namespace name="System" %]
-[%@ namespace name="System.Linq" %]
-[%@ namespace name="CodeFluent.Model" %]
+//[%@ reference name="C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.Core.dll" %]
+//[%@ namespace name="System" %]
+//[%@ namespace name="System.Linq" %]
+//[%@ namespace name="CodeFluent.Model" %]
 
 var context = {};
 context.indexedDB = {};
 context.indexedDB.db = null;
 
-context.indexedDB.open = function () {
+context.indexedDB.open = function (callback) {
     var version = 11;
-    var request = indexedDB.open([%=Producer.Project.DefaultNamespace%], version);
+    var request = indexedDB.open("[%=Producer.Project.DefaultNamespace%]", version);
 
     request.onupgradeneeded = function (e) {
         var db = e.target.result;
@@ -22,38 +22,42 @@ context.indexedDB.open = function () {
         }
 
         var store = db.createObjectStore("[%=entity.Name%]",
-          { keyPath: "id", autoIncrement: true });
+			[%foreach(Property property in entity.Properties){%]
+			    { keyPath: "[%=property.Name%]" [%=(property.IsPersistenceIdentity?", autoIncrement:	true":"")%] }[%=(entity.Properties.Last()==property?"":",")%]
+        //[%}%]
+		  );
 
-        store.createIndex([%=properties %], { unique: false });[%}%]
+        //[%}%]
     };
 
     request.onsuccess = function (e) {
         context.indexedDB.db = e.target.result;
+        if (callback !== undefined) callback();
     };
 
     request.onerror = context.indexedDB.onerror;
 };
-    [%foreach(Entity entity in Producer.Project.Entities){
-        string parameters = String.Join(", ", entity.Properties.Where(p => !p.IsPersistenceIdentity).Select(p => p.Name));%]
+[%foreach(Entity entity in Producer.Project.Entities){
+	string parameters = String.Join(", ", entity.Properties.Where(p => !p.IsPersistenceIdentity).Select(p => p.Name));%]
 context.indexedDB.[%=entity.Name%] = {}
 
-    context.indexedDB.[%=entity.Name%].add = function ([%= parameters %]) {
+context.indexedDB.[%=entity.Name%].add = function ([%= parameters %], callback) {
     var db = context.indexedDB.db;
     var trans = db.transaction(["[%=entity.Name%]"], "readwrite");
     var store = trans.objectStore("[%=entity.Name%]");
     var request = store.put({
-    [% 
-        foreach (Property property in entity.Properties.Where(p => !p.IsPersistenceIdentity)) {%]
-        "[%=property.Name%]": [%=property.Name%], [%}%]
-        "timeStamp": new Date().getTime()
-    });
+	[% 
+		foreach (Property property in entity.Properties.Where(p => !p.IsPersistenceIdentity)) {%]
+		"[%=property.Name%]": [%=property.Name%], [%}%]
+		"timeStamp": new Date().getTime()
+});
 
     request.onsuccess = function (e) {
-        console.log(e.value);
+        callback([%= parameters %],e.srcElement.result);
     };
 
     request.onerror = function (e) {
-        console.log(e.value);
+        console.log(e);
     };
 };
 
@@ -61,11 +65,10 @@ context.indexedDB.[%=entity.Name%].delete = function (id) {
     var db = context.indexedDB.db;
     var trans = db.transaction(["[%=entity.Name%]"], "readwrite");
     var store = trans.objectStore("[%=entity.Name%]");
-
     var request = store.delete(id);
 
     request.onsuccess = function (e) {
-        console.log(e);
+        //console.log(e);
     };
 
     request.onerror = function (e) {
@@ -73,7 +76,7 @@ context.indexedDB.[%=entity.Name%].delete = function (id) {
     };
 };
 
-context.indexedDB.[%=entity.Name%].loadAll = function () {
+context.indexedDB.[%=entity.Name%].loadAll = function (func) {
     var db = context.indexedDB.db;
     var trans = db.transaction(["[%=entity.Name%]"], "readwrite");
     var store = trans.objectStore("[%=entity.Name%]");
@@ -81,18 +84,26 @@ context.indexedDB.[%=entity.Name%].loadAll = function () {
     var keyRange = IDBKeyRange.lowerBound(0);
     var cursorRequest = store.openCursor(keyRange);
 
-    request.onsuccess = function (e) {
-        // not implemented
+    var allResults = [];
+
+    cursorRequest.onsuccess = function (e) {
+        var result = e.target.result;
+		
+        if(!!result == false)
+            return func(allResults);
+
+        allResults.push({ id: result.value.Id, text: result.value.Description, done: false });
+        result.continue();
     };
 
-    request.onerror = function (e) {
+    cursorRequest.onerror = function (e) {
         console.log(e);
     };
 };
 [%}%]
 
 function init() {
-    context.indexedDB.open(); // initialize the IndexDB context.
+    context.indexedDB.open(); // initialize the IndexedDB context.
 }
 
 window.addEventListener("DOMContentLoaded", init, false);
